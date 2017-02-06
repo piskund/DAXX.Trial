bd5f620fde076a8aabcfa7699848f77471a197d1
dip <dmytro.piskun@gmail.com>
2017-02-06 13:59:07 +0200

    Fix unit test according to async changes.

--- Daxx.Trial.MVC.Tests/Controllers/HomeControllerTests.cs
+++ Daxx.Trial.MVC.Tests/Controllers/HomeControllerTests.cs
@@ -1,11 +1,12 @@
 ﻿// -------------------------------------------------------------------------------------------------------------
-//  HomeControllerTest.cs created by DEP on 2017/02/04
+//  HomeControllerTests.cs created by DEP on 2017/02/06
 // -------------------------------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web.Mvc;
-using Daxx.Trial.MVC.Controllers;
+using Daxx.Trial.Abstractions.Entities;
 using Daxx.Trial.Abstractions.Interfaces;
+using Daxx.Trial.MVC.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -19,6 +20,9 @@ namespace Daxx.Trial.MVC.Tests.Controllers
         {
             // Arrange
             var repositoryMock = new Mock<ITradesRepository>();
+            repositoryMock.Setup(r => r.GetAllAsync())
+                .Returns(Task.FromResult(new List<TradeInfoEntity>() as IEnumerable<TradeInfoEntity>));
+
             var controller = new HomeController(repositoryMock.Object);
 
             // Act
